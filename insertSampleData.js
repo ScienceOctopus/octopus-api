b1cbc0cce3f961dab99c3beb72a1db3db62e890a
Vot <vot@siliconfen.co>
2019-09-13 23:45:03 +0100

    Add support for stages, add more sample data

--- insertSampleData.js
+++ insertSampleData.js
@@ -11,7 +11,7 @@ const user = {
 
 const problem = {
   id: 1,
-  title: 'Problem Ttile goes here',
+  title: 'Example Problem',
   summary: 'Problem Summary goes here',
   createdByUser: 1,
   dateCreated: '2019-09-01 00:00:00.000',
@@ -38,6 +38,69 @@ const publication = {
   createdByUser: 1,
   dateCreated: '2019-09-01 00:00:00.000',
   dateLastActivity: '2019-09-01 00:00:00.000',
+
+  tags: [],
+  stages: ['Hypotheses', 'Methods/Protocols', 'Data/Results', 'Analyses', 'Interpretations', 'Applications'],
+  collaborators: [
+    {
+      userID: 1,
+      role: 'author',
+      status: 'CONFIRMED',
+      dateAdded: '2019-09-01 00:00:00.000',
+    }
+  ]
+};
+
+const stages = [
+  {
+    name: 'Hypotheses',
+    singular: 'Hypothesis',
+    ratingCriteria: ['Well defined', 'Original', 'Scientifically valid'],
+  },
+  {
+    name: 'Methods/Protocols',
+    singular: 'Method/Protocol',
+    ratingCriteria: ['Details clear', 'Original', 'Appropriate test of hypothesis'],
+  },
+  {
+    name: 'Data/Results',
+    singular: 'Data/Result',
+    ratingCriteria: ['Well annotated', 'Followed protocol', 'Size of dataset'],
+  },
+  {
+    name: 'Analyses',
+    singular: 'Analysis',
+    ratingCriteria: ['Details clear', 'Original', 'Appropriate methodology'],
+  },
+  {
+    name: 'Interpretations',
+    singular: 'Interpretation',
+    ratingCriteria: ['Clearly written', 'Insightful', 'Consistent with data'],
+  },
+  {
+    name: 'Applications',
+    singular: 'Application',
+    ratingCriteria: ['Details clear', 'Impactful', 'Appropriate'],
+  },
+];
+
+const review = {
+  publicationId: 1,
+  ratingFirst: 5,
+  ratingSecond: 5,
+  ratingThird: 5,
+  userID: 5,
+  text: '',
+  dateCreated: '2019-09-01 00:00:00.000',
+};
+
+const file = {
+  id: 1234,
+  name: 'Test.docx',
+  size: 1231231,
+  userID: 1,
+  publicationID: 1,
+  type: 'publication'
 };
 
 //
@@ -74,4 +137,40 @@ Models.publications.insertOne(publication, (err, data) => {
   }
 
   console.log(`Inserted ${data.insertedCount} publication unit. ID: ${data.insertedId}`);
+});
+
+//
+
+console.log('Inserting file');
+
+Models.files.insertOne(file, (err, data) => {
+  if (err) {
+    console.log('Error:', err);
+  }
+
+  console.log(`Inserted ${data.insertedCount} file. ID: ${data.insertedId}`);
+});
+
+//
+
+console.log('Inserting review');
+
+Models.reviews.insertOne(review, (err, data) => {
+  if (err) {
+    console.log('Error:', err);
+  }
+
+  console.log(`Inserted ${data.insertedCount} review. ID: ${data.insertedId}`);
+});
+
+//
+
+console.log('Inserting sample stages');
+
+Models.stages.insertMany(stages, (err, data) => {
+  if (err) {
+    console.log('Error:', err);
+  }
+
+  console.log(`Inserted ${data.insertedCount} stages.`);
 });
