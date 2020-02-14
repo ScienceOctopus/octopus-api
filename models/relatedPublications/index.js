ffcf35cd1550c0caae98dd5aa53e1d9d5afa9ca7
Alexandru Patrascu <alexandru.patrascu@wingravity.com>
2020-02-14 13:13:45 +0200

    related publications - updates

---
+++ models/relatedPublications/index.js
@@ -0,0 +1,35 @@
+const RelatedPublicationsModel = require('../../lib/mongo').relatedPublications;
+
+function createRelatedPublication(data, callback) {
+  return RelatedPublicationsModel.insertOne(data, callback);
+}
+
+function updateRelatedPublication(data, callback) {
+  return RelatedPublicationsModel.updateOne({ _id: data._id }, data, callback);
+}
+
+function findRelatedPublications(query, callback) {
+  let mongoQuery =  { $and: [] };
+
+  if ( query.publicationID ) {
+    const publicationIDQuery = { publicationID: query.publicationID };
+    mongoQuery.$and.push(publicationIDQuery);
+  }
+
+  if ( query.relatedTo ) {
+    const relatedToQuery = { relatedTo: query.relatedTo };
+    mongoQuery.$and.push(relatedToQuery);
+  }
+
+  if (!mongoQuery.$and.length) {
+    mongoQuery = {};
+  }
+
+  return RelatedPublicationsModel.find(mongoQuery, callback);
+}
+
+module.exports = {
+  createRelatedPublication,
+  updateRelatedPublication,
+  findRelatedPublications,
+};
---
+++ routes/v1/relatedPublications.js
@@ -0,0 +1,105 @@
+const debug = require('debug');
+const _ = require('lodash');
+
+const RelatedPublicationsModel = require('../../models/relatedPublications');
+const ObjectID = require('../../lib/mongo').ObjectID;
+
+/**
+ * @api {post} /v1/relatedPublications/createRelatedPublication Create a Related Publication
+ * @apiName createRelatedPublication
+ * @apiGroup RelatedPublications
+ * @apiVersion 1.0.0
+ * @apiSampleRequest /v1/relatedPublications/createRelatedPublication
+ *
+ * @apiParam {Object} data RelatedPublication's data.
+ *
+ * @apiUse RelatedPublicationObject
+ */
+
+function createRelatedPublication(req, res) {
+  const relatedPublicationData = _.merge({}, req.body);
+
+  return RelatedPublicationsModel.createRelatedPublication(
+    relatedPublicationData,
+    (relatedPublicationErr, relatedPublicationResult) => {
+      if (relatedPublicationErr) {
+        debug('octopus:api:error')(`Error in createRelatedPublication: ${relatedPublicationErr}`);
+        debug('octopus:api:error')(`Error in createRelatedPublication data: ${relatedPublicationData}`);
+        return res.send('ERROR');
+      }
+
+      return res.json(relatedPublicationResult);
+    }
+  );
+}
+
+/**
+ * @api {post} /v1/relatedPublications/updateRelatedPublication Update a Related Publication
+ * @apiName updateRelatedPublication
+ * @apiGroup RelatedPublications
+ * @apiVersion 1.0.0
+ * @apiSampleRequest /v1/relatedPublications/updateRelatedPublication
+ *
+ * @apiParam {Object} data RelatedPublication's data.
+ *
+ * @apiUse RelatedPublicationObject
+ */
+
+function updateRelatedPublication(req, res) {
+  const relatedPublicationData = _.merge({}, req.body);
+  relatedPublicationData._id = ObjectID(relatedPublicationData._id);
+
+  return RelatedPublicationsModel.updateRelatedPublication(
+    relatedPublicationData,
+    (relatedPublicationErr, relatedPublicationResult) => {
+      if (relatedPublicationErr) {
+        debug('octopus:api:error')(`Error in updateRelatedPublication: ${relatedPublicationErr}`);
+        debug('octopus:api:error')(`Error in updateRelatedPublication data: ${relatedPublicationData}`);
+        return res.send('ERROR');
+      }
+
+      return res.json(relatedPublicationResult);
+    }
+  );
+}
+
+/**
+ * @api {get} /v1/relatedPublications/find Find RelatedPublications based on specified criteria
+ * @apiName findRelatedPublications
+ * @apiGroup RelatedPublications
+ * @apiVersion 1.0.0
+ * @apiSampleRequest /v1/relatedPublications/find
+ *
+ * @apiParam {Number} id RelatedPublication's unique ID.
+ * @apiParam {Number} publicationID of RelatedPublication's publicationID.
+ * @apiParam {Number} relatedTo of RelatedPublication's relatedTo.
+ *
+ * @apiSuccess {Array} results Array containing Related Publications matching the specified criteria
+ */
+
+function findRelatedPublications(req, res) {
+  const publicationID = String(req.params.id);
+
+  const query = {
+    publicationID: req.query.publicationID,
+    relatedTo: req.query.relatedTo
+  };
+
+  return RelatedPublicationsModel.findRelatedPublications(
+    query,
+    (relatedPublicationErr, relatedPublicationData) => {
+      if (relatedPublicationErr) {
+        debug('octopus:api:error')(`Error in findRelatedPublications for ${publicationID}: ${relatedPublicationErr}`);
+        return res.send('ERROR');
+      }
+
+      return res.json(relatedPublicationData);
+    }
+  );
+}
+
+module.exports = {
+  createRelatedPublication,
+  updateRelatedPublication,
+  findRelatedPublications
+};
